384d0e379814564d1ea140e80311bae92bfc1af8
TeodoraAlina <alina.teower@gmail.com>
2023-10-05 12:03:10 +0000

    Build: PostList view and functionality to list and create a post

--- posts/serializers.py
+++ posts/serializers.py
@@ -31,5 +31,5 @@ class PostSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
             'title', 'description', 'image', 'location',
-            'tags', 'enviromental_metrics'
+            'tags', 'environmental_metrics'
         ]
