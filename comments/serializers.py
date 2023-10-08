abb1a000170549cb77ecca977b0073cd1131ccfb
TeodoraAlina <alina.teower@gmail.com>
2023-10-08 16:36:50 +0000

    Build: date and time formatting for comments

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Comment
+from django.contrib.humanize.templatetags.humanize import naturaltime
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -7,6 +8,9 @@ class CommentSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
+
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -18,6 +22,13 @@ class CommentSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
             'post', 'created_at', 'updated_at', 'content'
         ]
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+    
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
+
 
 
 class CommentDetailSerializer(CommentSerializer):
--- drf_api/settings.py
+++ drf_api/settings.py
@@ -79,9 +79,10 @@ REST_FRAMEWORK = {
             if 'DEV' in os.environ
             else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
         )
-    ]
+    ],
     'DEFAULT_PAGINATION_CLASS':  'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%d %b %Y'
 }
 
 if 'DEV' not in os.environ:
@@ -183,3 +184,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TAGGIT_CASE_INSENSITIVE = True
