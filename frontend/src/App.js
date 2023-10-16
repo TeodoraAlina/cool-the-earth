697409eb5bbe65ef48fffeb28ee0c5e99ec39912
TeodoraAlina <alina.teower@gmail.com>
2023-10-16 06:57:45 +0000

    Style: create, import and apply css files for App and NavBar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
-import Button from 'react-bootstrap/Button'
+import styles from './App.module.css'
 import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <NavBar />
     </div>
   );
