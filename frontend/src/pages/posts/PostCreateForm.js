c78211bcc29eb15c90403ee577b2866c44014426
TeodoraAlina <alina.teower@gmail.com>
2023-10-17 12:14:55 +0000

    Build: on submit and errors alert functionality for PostCreateForm

--- frontend/src/pages/posts/PostCreateForm.js
+++ frontend/src/pages/posts/PostCreateForm.js
@@ -1,18 +1,23 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Alert from "react-bootstrap/Alert";
+import Image from "react-bootstrap/Image";
+import Asset from "../../components/Asset";
 
 import Upload from "../../assets/upload.jpeg";
 
 import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
-import Asset from "../../components/Asset";
-import { Image } from "react-bootstrap";
+
+import { useHistory } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+
 
 function PostCreateForm() {
   const [errors, setErrors] = useState({});
@@ -21,8 +26,14 @@ function PostCreateForm() {
     title: "",
     description: "",
     image: "",
+    tags: "",
+    location: "",
+    environmental_metrics: "",
   });
-  const { title, description, image } = postData;
+  const { title, description, image, tags, location, environmental_metrics } = postData;
+
+  const imageInput = useRef(null);
+  const history = useHistory();
 
   const handleChange = (event) => {
     setPostData({
@@ -30,6 +41,28 @@ function PostCreateForm() {
       [event.target.name]: event.target.value,
     });
   };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    
+    formData.append("title", title)
+    formData.append("description", description);
+    formData.append('tags', tags)
+    formData.append('location', location)
+    formData.append("image", imageInput.current.files[0]);
+    formData.append('environmental_metrics', environmental_metrics)
+
+    try {
+        const { data } = await axiosReq.post("/posts/", formData);
+        history.push(`/posts/${data.id}`);
+    } catch (err) {
+        console.log(err);
+        if (err.response?.status !== 401) {
+            setErrors(err.response?.data);
+        }
+    }
+};
 
   const handleChangeImage = (event) => {
     if (event.target.files.length) {
@@ -52,6 +85,11 @@ function PostCreateForm() {
           onChange={handleChange}
         />
       </Form.Group>
+      {errors?.content?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
       <Form.Group>
         <Form.Label>Description</Form.Label>
         <Form.Control
@@ -62,10 +100,57 @@ function PostCreateForm() {
           onChange={handleChange}
         />
       </Form.Group>
+      {errors?.content?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+      <Form.Group>
+        <Form.Label>Tags</Form.Label>
+        <Form.Control
+          type="text"
+          name="tags"
+          value={tags}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      {errors?.tags?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+      <Form.Group>
+        <Form.Label>Location</Form.Label>
+        <Form.Control
+          type="text"
+          name="location"
+          value={location}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      {errors?.tags?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
+      <Form.Group>
+        <Form.Label>Environmental Metrics</Form.Label>
+        <Form.Control
+          type="text"
+          name="environmental_metrics"
+          value={environmental_metrics}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      {errors?.tags?.map((message, idx) => (
+        <Alert variant="warning" key={idx}>
+          {message}
+        </Alert>
+      ))}
 
       <Button
         className={`${btnStyles.Button} ${btnStyles.Green}`}
-        onClick={() => {}}
+        onClick={() => history.goBack()}
       >
         cancel
       </Button>
@@ -76,7 +161,7 @@ function PostCreateForm() {
   );
 
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Row>
         <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
           <Container
@@ -113,6 +198,7 @@ function PostCreateForm() {
                 id="image-upload"
                 accept="image/*"
                 onChange={handleChangeImage}
+                ref={imageInput}
               />
             </Form.Group>
             <div className="d-md-none">{textFields}</div>
