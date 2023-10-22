5ffba717d3333616b78489c8510838e6da94a79f
TeodoraAlina <alina.teower@gmail.com>
2023-10-22 05:27:36 +0000

    Fix: Issue with not receiving tags data

--- posts/serializers.py
+++ posts/serializers.py
@@ -4,7 +4,7 @@ from taggit.serializers import (TagListSerializerField, TaggitSerializer)
 from likes.models import Like
 
 
-class PostSerializer(serializers.ModelSerializer):
+class PostSerializer(TaggitSerializer, serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
