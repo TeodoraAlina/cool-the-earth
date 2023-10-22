88da7bf689a36ffa4ec6f3c33557924867709496
TeodoraAlina <alina.teower@gmail.com>
2023-10-22 09:34:00 +0000

    Build: Comment component and associated css

--- frontend/src/pages/events/EventPage.js
+++ frontend/src/pages/events/EventPage.js
@@ -2,9 +2,6 @@ import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
-
-import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Event from "./Event";
@@ -34,7 +31,6 @@ function EventPage() {
             <Col className="py-2 p-0 p-lg-2" lg={8}>
                 <p>Most followed for mobile</p>
                 <Event {...event.results[0]} setEvents={setEvent} eventPage />
-                <Container className={appStyles.Content}>Comments</Container>
             </Col>
             <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
                 Most followed for desktop
