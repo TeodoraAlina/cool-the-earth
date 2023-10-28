2757da7c11a37da8fb229e66f089c302adc0f98e
TeodoraAlina <alina.teower@gmail.com>
2023-10-28 18:53:28 +0000

    Test: Create tests for profiles and eventgo

--- eventgo/tests.py
+++ eventgo/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Event
+from .models import Going
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class GoingListViewTests(APITestCase):
+    """
+    Tests for the Going model list view
+    """
+
+    def setUp(self):
+        sophie = User.objects.create_user(username="sophie", password="pass")
+        event_a = Event.objects.create(
+            owner=sophie, title="an event", event_date="2030-04-01"
+        )
+
+    def test_can_list_going(self):
+        sophie = User.objects.get(username="sophie")
+        event_a = Event.objects.get(id=1)
+        Going.objects.create(owner=sophie, event=event_a)
+        response = self.client.get("/eventgo/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+
+class GoingDetailViewTests(APITestCase):
+    """
+    Tests for the Going model detail view
+    """
+
+    def setUp(self):
+        sophie = User.objects.create_user(username="sophie", password="pass")
+        stefan = User.objects.create_user(username="stefan", password="pass")
+        event_a = Event.objects.create(
+            owner=sophie, title="an event", event_date="2030-04-01"
+        )
+        event_b = Event.objects.create(
+            owner=stefan, title="another event", event_date="2030-04-01"
+        )
+        Going.objects.create(owner=sophie, event=event_a)
+        Going.objects.create(owner=stefan, event=event_b)
+
+    def test_cant_retrieve_going_using_invalid_id(self):
+        response = self.client.get("/eventgo/999/")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_can_retrieve_going_using_valid_id(self):
+        response = self.client.get("/eventgo/1/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ProfileListViewTests(APITestCase):
+    """
+    Tests for the Profile model list view
+    """
+
+    def setUp(self):
+        sophie = User.objects.create_user(username="sophie", password="pass")
+        stefan = User.objects.create_user(username="stefan", password="pass")
+
+    def test_profile_automatically_created_on_user_creation(self):
+        response = self.client.get("/profiles/")
+        count = Profile.objects.count()
+        self.assertEqual(count, 2)
+
+    def test_can_list_profiles(self):
+        response = self.client.get("/profiles/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class ProfileDetailViewTests(APITestCase):
+    """
+    Tests for the Profile model detail view
+    """
+
+    def setUp(self):
+        sophie = User.objects.create_user(username="sophie", password="pass")
+        stefan = User.objects.create_user(username="stefan", password="pass")
+
+    def test_cant_retrieve_profile_using_invalid_id(self):
+        response = self.client.get("/profiles/999/")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_can_retrieve_profile_using_valid_id(self):
+        response = self.client.get("/profiles/2/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
